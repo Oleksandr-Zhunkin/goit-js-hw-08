4db5825d130b9e5912997a32608d6837bc98182a
Oleksandr_Zhynkin <godinsighted@gmail.com>
2024-05-10 18:56:22 +0300

    star

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-const categoriesEl = document.querySelectorAll('.item');
-console.log(`Number of categories: ${categoriesEl.length}`);
-
-categoriesEl.forEach(elem => {
-  console.log(`Category: ${elem.firstElementChild.textContent}`);
-  console.log(`Elements: ${elem.lastElementChild.childElementCount}`);
-});
